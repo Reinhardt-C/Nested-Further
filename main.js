bf365e1045095ceef4830642ee5f7310401a1a35
Logan McDonald <loganmcdonald3141@gmail.com>
2020-04-26 10:49:50 +1200

    v0.1.0 The Human Update

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ function init() {
 		.then(json => {
 			obj = json.objects;
 			prop = json.properties;
-			document.body.innerHTML = "Nested Further - v0.0.0<hr>";
+			document.body.innerHTML = "Nested Further - v0.1.0<hr>";
 			createBase();
-			createNode("universe");
+			createNode("universe", "everything");
 			return json;
 		});
 	// .then(json => console.log(json));
@@ -16,13 +16,26 @@ function init() {
 
 init();
 
+// Consonants
+// ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "R", "S", "T", "V", "W", "X", "Y", "Z", "Th", "Sh", "Ch", "Sm", "Sn", "St", "Sw", "Sk", "Sl", "Sp", "Sf", "Thw", "Dw", "Tw", "Thr", "Dr", "Tr", "Qu", "Cr", "Cl", "Pr", "Fr", "Br", "Gr", "Pl", "Fl", "Bl", "Gl", "Shr", "Spl", "Spr", "Str", "Scr", "Squ"]
+// ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z", "th", "sh", "ch", "sm", "sn", "st", "sw", "sk", "sl", "sp", "sf", "thw", "dw", "tw", "thr", "dr", "tr", "qu", "cr", "cl", "pr", "fr", "br", "gr", "pl", "fl", "bl", "gl", "shr", "spl", "spr", "str", "scr", "squ"]
+// Vowels
+// ["A", "E", "I", "O", "U", "Ae", "Ai", "Ao", "Ei", "Ee", "Oi", "Ou", "Oo"]
+// ["a", "e", "i", "o", "u", "ae", "ai", "ao", "ei", "ee", "oi", "ou", "oo"]
+// Both
+// ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "R", "S", "T", "V", "W", "X", "Y", "Z", "Th", "Sh", "Ch", "Sm", "Sn", "St", "Sw", "Sk", "Sl", "Sp", "Sf", "Thw", "Dw", "Tw", "Thr", "Dr", "Tr", "Qu", "Cr", "Cl", "Pr", "Fr", "Br", "Gr", "Pl", "Fl", "Bl", "Gl", "Shr", "Spl", "Spr", "Str", "Scr", "Squ", "A", "E", "I", "O", "U", "Ae", "Ai", "Ao", "Ei", "Ee", "Oi", "Ou", "Oo"]
+// ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "y", "z", "th", "sh", "ch", "sm", "sn", "st", "sw", "sk", "sl", "sp", "sf", "thw", "dw", "tw", "thr", "dr", "tr", "qu", "cr", "cl", "pr", "fr", "br", "gr", "pl", "fl", "bl", "gl", "shr", "spl", "spr", "str", "scr", "squ", "a", "e", "i", "o", "u", "ae", "ai", "ao", "ei", "ee", "oi", "ou", "oo"]
+
 function createNode(id, parent = "everything", props = "") {
 	let x = getFreeId();
 
 	let span = $$("span");
 	span.className = "unopened";
 	span.id = "s" + x;
-	span.innerText = obj[id].name;
+	if (obj[id].randName) {
+		let name = randName(obj[id].randName);
+		span.innerText = name;
+	} else span.innerText = obj[id].name;
 	span.setAttribute("data-props", props);
 	let s = props.split(/\s/);
 	try {
@@ -31,7 +44,8 @@ function createNode(id, parent = "everything", props = "") {
 		if (s[0] != "") console.error("Unknown property!");
 		s = [];
 	}
-	if (s.length > 0) span.innerText = s.join(" ") + " " + span.innerText;
+	s = s.filter(e => e !== "");
+	if (s.length > 0) span.innerText = s.join(" ") + (s.join(" ").length > 0 ? " " : "") + span.innerText;
 
 	let li = $$("li");
 	let ul = $$("ul");
@@ -56,6 +70,15 @@ function createNode(id, parent = "everything", props = "") {
 
 	$(parent).appendChild(li);
 	return span;
+}
+
+function randName(pattern) {
+	let name = "";
+	for (let i of pattern) {
+		let temp = chooseRandom(i);
+		if (!name.endsWith(" ") || /^[A-Z]/.test(temp)) name += temp;
+	}
+	return name;
 }
 
 function handleChildren(id, parent) {
@@ -190,6 +213,10 @@ function chooseWeighted(items) {
 	chances = chances.map(el => (acc = el + acc));
 	var rand = Math.random() * sum;
 	return items[chances.filter(el => el <= rand).length] || items[0];
+}
+
+function chooseRandom(array) {
+	return array[randBetween(0, array.length)];
 }
 
 function randBetween(min, max) {
